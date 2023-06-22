00485cf778bb44f00bdf71535a47fb9266f354c0
saeb-faraji-gargari <saebfaraji@gmail.com>
2023-06-22 22:09:21 +0200

    multiply function is added

--- geo_test.py
+++ geo_test.py
@@ -1,8 +1,13 @@
 from geo import add 
 from geo import subtract
+from geo import multiply
 
 def test_add():
     assert add (13 , 17) == 30
 
 def test_subtract():
     assert subtract (50 , 20) == 30
+
+def multiply():
+    assert multiply (25 , 2) == 50   
+    
