f1b4a211c3ec7bb582e2ca7393345e1c5582cf7c
komar1987 <kjavanmardi@ihu.edu.gr>
2023-06-23 10:12:14 +0200

    add the devide function

--- geo_test.py
+++ geo_test.py
@@ -1,8 +1,12 @@
 from geo import add 
 from geo import subtract
+from geo import devide
 
 def test_add():
     assert add (13 , 17) == 30
 
 def test_subtract():
     assert subtract (50 , 20) == 30
+
+def test_subtract():
+    assert subtract (70 , 10) == 7
