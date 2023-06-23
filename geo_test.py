823902c07da2ec94537c6338b90a4241d5a63851
komar1987 <kjavanmardi@ihu.edu.gr>
2023-06-23 13:59:25 +0200

    the abc func added

--- geo_test.py
+++ geo_test.py
@@ -1,6 +1,7 @@
 from geo import add 
 from geo import subtract
 from geo import devide
+from geo import multiply
 
 def test_add():
     assert add (13 , 17) == 30
@@ -10,3 +11,7 @@ def test_subtract():
 
 def test_devide():
     assert devide (70 , 10) == 7
+
+def test_multiply():
+    assert multiply(10 , 11 , 2) == 220
+    
